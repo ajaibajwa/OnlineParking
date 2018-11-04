8e81e9494630210a6a62faf25d8a8bc31d70cc94
AJAY BAJWA <AJAY-MAC@ajays-macbook-pro.local>
2018-11-04 15:34:06 -0500

    Initial Commit

---
+++ Online Parking Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Online Parking Project
+//
+//  Created by AJAY BAJWA on 2018-11-04.
+//  Copyright © 2018 lambton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
